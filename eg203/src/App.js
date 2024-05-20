aa54f2a99e50d97a1b205ba15765e80ed5ee3f69
Your NameKaminariSora <nonthacha.h@gmail.com>
2024-05-20 20:31:17 +0700

    Update App.js

--- eg203/src/App.js
+++ eg203/src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './App.css';
 import Home from './home/home_template';
-import Section2 from './Components_2/section2';
-
-import Section5 from './Components_5/section5';
 
 function App() {
   return (
